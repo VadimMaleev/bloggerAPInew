a4a3b9890753c476a5e969e544f602d6dea91113
VadimMaleev <maleeffset@yandex.by>
2022-07-22 12:32:44 +0300

    add access and refresh tokens (fix cookies - secure true)

--- src/middlewares/authorization-middware.ts
+++ src/middlewares/authorization-middware.ts
@@ -35,13 +35,13 @@ export const jwtRefreshAuthMiddleware = async (req: Request, res: Response, next
     const refreshTokenFromCookie = req.cookies?.refreshToken
     console.log(req.headers.cookie)
     if (!refreshTokenFromCookie) {
-        res.send(401)
+        res.status(401).send('cookies is not exist')
         return
     }
 
     const refreshToken = await jwtService.findExpiredToken(refreshTokenFromCookie)
     if (refreshToken?.refreshToken) {
-        return res.sendStatus(401)
+        return res.status(401).send("token is expired")
     } else {
         const token = refreshTokenFromCookie.split(' ')[1]
         const _userId = await jwtService.extractUserIdFromToken(token)
@@ -50,7 +50,7 @@ export const jwtRefreshAuthMiddleware = async (req: Request, res: Response, next
             req.user = await usersService.findUserById(userId)
             next()
         } else {
-            return res.send(401)
+            return res.status(401).send("user not exist")
         }
     }
 
